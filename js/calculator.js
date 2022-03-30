4e61c09a7be084c7013da38767f1ce62d53b6440
Lopunnii <kimi7@xtra.co.nz>
2022-03-30 17:26:58 +0900

    Add click function to each number button

--- js/calculator.js
+++ js/calculator.js
@@ -17,3 +17,63 @@ const divide = function (num1, num2) {
 const operate = function (operator, num1, num2) {
      return operator(num1, num2); 
 }
+
+const clickNumberButton = function() {
+    const zeroButton = document.querySelector('.zero');
+    zeroButton.addEventListener('click', () => {
+        const zero = 0;
+        display(zero);
+    });
+    const oneButton = document.querySelector('.one');
+    oneButton.addEventListener('click', () => {
+        const one = 1;
+        display(one);
+    });
+    const twoButton = document.querySelector('.two');
+    twoButton.addEventListener('click', () => {
+        const two = 2;
+        display(two);
+    });
+    const threeButton = document.querySelector('.three');
+    threeButton.addEventListener('click', () => {
+        const three = 3;
+        display(three);
+    });
+    const fourButton = document.querySelector('.four');
+    fourButton.addEventListener('click', () => {
+        const four = 4;
+        display(four);
+    });
+    const fiveButton = document.querySelector('.five');
+    fiveButton.addEventListener('click', () => {
+        const five = 5;
+        display(five);
+    });
+    const sixButton = document.querySelector('.six');
+    sixButton.addEventListener('click', () => {
+        const six = 6;
+        display(six);
+    });
+    const sevenButton = document.querySelector('.seven');
+    sevenButton.addEventListener('click', () => {
+        const seven = 7;
+        display(seven);
+    });
+    const eightButton = document.querySelector('.eight');
+    eightButton.addEventListener('click', () => {
+        const eight = 8;
+        display(eight);
+    });
+    const nineButton = document.querySelector('.nine');
+    nineButton.addEventListener('click', () => {
+        const nine = 9;
+        display(nine);
+    });
+}
+clickNumberButton(); 
+
+
+let display = function(number) {
+    const display = document.querySelector('.display');
+    display.textContent = number;
+}
